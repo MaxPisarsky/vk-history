153c4de77370c91be4878f2079b06014ee43ac03
German Shabalin <german.shabalin@ms2w.com>
2016-01-14 17:31:01 +0500

    iterate through the dialogs

--- extension/js/background.js
+++ extension/js/background.js
@@ -6,11 +6,69 @@ var pollInterval = 1000 * 60;
 
 function pollHistory() {
 	VK.checkAuth(function(token) {
-		VK.getDialogs(token, 0, 200, function(resp){
+		VK.getDialogs(token, 0, 200, function(resp) {
 			console.log(resp);
 		});
 	});
 	window.setTimeout(pollHistory, pollInterval);
 }
 
-pollHistory();
+function processChunkDialogs(page, chunkHandler) {
+	VK.checkAuth(function(token) {
+		VK.getDialogs(token, page * VK.MAX_DIALOGS_ON_PAGE, VK.MAX_DIALOGS_ON_PAGE, function(resp) {
+			var items = resp && resp.response && resp.response.items;
+			if (items && items.length && chunkHandler) {
+				chunkHandler(page, items);
+			}
+		});
+	});
+}
+
+function processAllDialogs() {
+	processChunkDialogs(0, createChunkHandler);
+}
+
+function createChunkHandler(page, items) {
+	return function(page, items) {
+		console.log(page);
+		console.log(items);
+		if (items) {
+			items.forEach(function(dialog) {
+				processDialog(dialog);
+			});
+		}
+		processChunkDialogs(page + 1, createChunkHandler);
+	} (page, items);
+}
+
+function processDialogPage(dialogId, page, chunkHandler) {
+	VK.checkAuth(function(token) {
+		VK.getDialog(token, dialogId, page * VK.MAX_DIALOGS_ON_PAGE, VK.MAX_DIALOGS_ON_PAGE, function(resp) {
+			var items = resp && resp.response && resp.response.items;
+			if (items && items.length && chunkHandler) {
+				chunkHandler(dialogId, page, items);
+			}
+		});
+	});
+}
+
+function processDialog(dialog) {
+	var dialogId = VK.getDialogId(dialog);
+	processDialogPage(dialogId, 0, createDialogHandler)
+}
+
+function createDialogHandler(dialogId, page, items) {
+	return function(dialogId, page, items) {
+		console.log(dialogId);
+		console.log(page);
+		console.log(items);
+		/*if (items) {
+			items.forEach(function(dialog) {
+				processDialog(dialog);
+			});
+		}*/
+		processDialogPage(dialogId, page + 1, createDialogHandler);
+	} (dialogId, page, items);
+}
+
+processAllDialogs();
--- extension/js/vk-api.js
+++ extension/js/vk-api.js
@@ -95,6 +95,10 @@ const VK = (function() {
 				if (resp && resp.error && resp.error.error_code === 5) {
 					console.log('token expired');
 					chrome.storage.sync.remove('vkaccess_token', checkAuth(function(token) { getDialogs(token, offset, count, callback); }));
+				} else if (resp && resp.error && resp.error.error_code === 6) {
+					setTimeout(function() {
+						getDialogs(token, offset, count, callback);
+					}, 334);
 				} else {
 					callback && callback(resp);
 				}
@@ -104,10 +108,48 @@ const VK = (function() {
 		xhr.send();
 	}
 
+	function getDialogId(dialog) {
+		var message = dialog && dialog.message;
+		var dialogId;
+		if (message) {
+			dialogId = message.user_id;
+			if (message.chat_id) {
+				dialogId = message.chat_id + 2000000000;
+			}
+			var date = message.date;
+			console.log(dialogId, date);
+		}
+		return dialogId;
+	}
+
+	function getDialog(token, id, offset, count, callback, start_message_id) {
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				var resp = JSON.parse(xhr.responseText);
+				if (resp && resp.error && resp.error.error_code === 5) {
+					console.log('token expired');
+					chrome.storage.sync.remove('vkaccess_token', checkAuth(function(token) { getDialog(token, id, offset, count, callback, start_message_id); }));
+				} else if (resp && resp.error && resp.error.error_code === 6) {
+					setTimeout(function() {
+						getDialog(token, id, offset, count, callback, start_message_id);
+					}, 334);
+				} else {
+					callback && callback(resp);
+				}
+			}
+		};
+		xhr.open("GET", vk_api + "messages.getHistory?" + (start_message_id === undefined ? "rev=1" : "") + (id > 2000000000 ? "&peer_id=" + id : ("&user_id=" + id)) + (start_message_id ? "&start_message_id=" + start_message_id : "") + "&offset=" + ((start_message_id ? -1 : 1) * offset) + "&count=" + count + "&v=5.41&access_token=" + token, true);
+		xhr.send();
+	}
+
 	return class VK {
 		static checkAuth(tokenCallback) { checkAuth(tokenCallback); }
 		static getUser(token, callback) { getUser(token, callback); }
 		static getDialogs(token, offset, count, callback) { getDialogs(token, offset, count, callback); }
+		static getDialogId(dialog) { return getDialogId(dialog); }
+		static getDialog(token, id, offset, count, callback, start_message_id) { getDialog(token, id, offset, count, callback, start_message_id); }
+		static get MAX_DIALOGS_ON_PAGE() { return 200; }
 	}
 })();
 
